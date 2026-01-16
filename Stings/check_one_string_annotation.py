26c955d372fe9289e34a6caedaf62fe7d734eafe
Eshaanm4964 <eshaanm4964@gmail.com>
2026-01-16 18:38:15 +0530

    trick one

---
+++ Stings/check_one_string_annotation.py
@@ -0,0 +1,10 @@
+def rotateString(self, s: str, goal: str) -> bool:
+    if len(s) != len(goal):
+        return False  
+    for i in range(len(s)):
+        rotated = s[i:] + s[:i]  
+        if rotated == goal:
+            return True  
+    return False  
+
+# rotation tionrota
