95b2f1be1dc1255611504a2cff45756d4bbb0cb1
Eshaanm4964 <eshaanm4964@gmail.com>
2026-01-26 22:39:15 +0530

    Update var

--- DSA basic problems/problem06.py
+++ DSA basic problems/problem06.py
@@ -1,6 +1,6 @@
 def checkPrime(n):
-    cnt = 0 
+    count = 0 
     for i in range(1, n + 1):
         if n % i == 0:
-            cnt += 1
-    return cnt == 2
+            count += 1
+    return count == 2
